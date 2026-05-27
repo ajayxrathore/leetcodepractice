170bd7a90410d890f4785556b94a666b86b6f518
Ajay Rathore <ajayrathore3200@gmail.com>
2026-05-27 20:44:41 +0530

    9.js

---
+++ 9.js
@@ -0,0 +1,17 @@
+// palindrome
+function isPalindrome(num){
+    if(num<0){
+        return false
+    }
+    let str = num.toString()
+    let left = 0
+    let right =str.length-1
+    while(left<right){
+        if(str[left]!=str[right]){
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
