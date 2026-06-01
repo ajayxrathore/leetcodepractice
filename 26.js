49489a09f85f960fd0d075463c91c66819841c83
Ajay Rathore <ajayrathoref@gmail.com>
2026-06-01 22:06:27 +0530

    26.js

---
+++ 26.js
@@ -0,0 +1,12 @@
+// remove duplicates from sorted array
+function removeDuplicates(nums) {
+    let i = 0
+    for(let j=1;j<nums.length;j++){
+        if(nums[i]!==nums[j]){
+            i++;
+            nums[i]=nums[j]
+        }
+    }
+    return i+1;
+};
+
